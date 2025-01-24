41b6a6ae620d848d573e3c73fbc69b59c59bb561
Mikuedgy <villalobosantiag@gmail.com>
2025-01-24 16:07:20 -0500

    update menu pulido

--- scripts/main.py
+++ scripts/main.py
@@ -8,30 +8,36 @@ clock = pygame.time.Clock()
 height, width = screen.get_size()
 
 #Font and colors
-font = pygame.font.Font('fonts\Pixeltype.ttf',90)
+font = pygame.font.Font('fonts\Concrete.ttf',90)
 
 black = (23, 32, 42)
 white = (253, 254, 254)
 
+#Sound effects
+menu_sound = pygame.mixer.Sound("effects\menusound.wav")
+
 
 #Background
-surface_load_background = pygame.image.load('graphics/background.jpg')
+surface_load_background = pygame.image.load('graphics/background.png')
 surface_background = pygame.transform.scale(surface_load_background,(height, width))
 rect_background = surface_background.get_rect(midbottom = (height//2, width))
 
 #Title text
 surface_load_title = pygame.image.load('graphics/title.png')
-surface_title = pygame.transform.scale(surface_load_title,(height//1.5,width//1.5))
+surface_title = pygame.transform.smoothscale(surface_load_title,(height//1.5,width//1.5))
 rect_title = surface_title.get_rect(center = (height//2,width//3))
 
 #Play
-surface_play = font.render('Play', False, white)
+surface_play = font.render('Play', True, white)
 rect_play = surface_play.get_rect(center = (height//2,width//1.4))
 
 #Exit
-surface_exit = font.render('Exit', False, white)
+surface_exit = font.render('Exit', True, white)
 rect_exit = surface_exit.get_rect(center = (height//2, width//1.2))
 
+#Switches
+sound_played_play = False
+sound_played_exit = False
 
 while True:
     #Loop events
@@ -46,7 +52,8 @@ while True:
                     exit()
                 if rect_play.collidepoint(event.pos):
                     print('JUGANDO')
-        
+
+
     #Background
     screen.blit(surface_background, rect_background)
 
@@ -57,10 +64,26 @@ while True:
     screen.blit(surface_play, rect_play)
     screen.blit(surface_exit, rect_exit)
 
+
+
     #Button mouse motion animation
     mouse_pos = pygame.mouse.get_pos()
-    if rect_play.collidepoint(mouse_pos):pygame.draw.line(screen,white, (rect_play.left, rect_play.bottom),(rect_play.right, rect_play.bottom), 4)
-    if rect_exit.collidepoint(mouse_pos):pygame.draw.line(screen,white, (rect_exit.left, rect_exit.bottom - 10),(rect_exit.right, rect_exit.bottom -10), 4)
+    if rect_play.collidepoint(mouse_pos):
+        pygame.draw.line(screen,white, (rect_play.left, rect_play.bottom +10 ),(rect_play.right, rect_play.bottom +10), 7)
+        if not sound_played_play:  
+            menu_sound.play()
+            sound_played_play = True
+    else:
+        sound_played_play = False
+    
+    if rect_exit.collidepoint(mouse_pos):
+        pygame.draw.line(screen,white, (rect_exit.left, rect_exit.bottom - 5),(rect_exit.right, rect_exit.bottom -5), 7)
+        if not sound_played_exit:  
+            menu_sound.play()
+            sound_played_exit = True
+    else:
+        sound_played_exit = False
+        
 
 
     pygame.display.update()
